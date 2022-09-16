97da47333a4a1482ebc79b591de0a874bdca23d0
rickypazd <rickypazd@hotmail.com>
2022-09-16 05:01:31 -0400

    as

--- compile.sh
+++ compile.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+ELM=$(cat pom.xml | grep "version" | cut -d'>' -f2 | cut -d'<' -f1)
+a=(${ELM[@]})
+version=${a[0]}
+
 name=${PWD##*/}
+
 mvn clean compile assembly:single
 rm -fr ./bin
 mkdir -pv ./bin
@@ -9,6 +14,9 @@ for obj in $arr_jar; do
     $(jar xf ../$obj)
 done
 cd ../
-jar cfM "$name.jar" -C bin/ .  # //TODOD  -C classPath
+jar cfM "$name.jar" -C bin/ . # //TODOD  -C classPath
 rm -fr bin
+
+scp "$name.jar" "root@147.182.209.156:/root/fourteam/nginx/html/downloads/jars/${name}/${name}-latest.jar"
+scp "$name.jar" "root@147.182.209.156:/root/fourteam/nginx/html/downloads/jars/${name}/${name}-${version}.jar"
 
---
+++ updateLibs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://147.182.209.156/downloads/jars/Fourteam/Fourteam-latest.jar
+mv Fourteam-latest.jar ./lib/
