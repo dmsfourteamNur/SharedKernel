00c9c99e0a0b1b4083bf2673a55baddaeae98558
rickypazd <rickypazd@hotmail.com>
2022-09-20 01:35:49 -0400

    as

--- src/main/java/core/ConfirmedDomainEvent.java
+++ src/main/java/core/ConfirmedDomainEvent.java
@@ -2,11 +2,11 @@ package core;
 
 import Fourteam.mediator.Notification;
 
-public class ConfirmedDomainEvent<T> extends DomainEvent implements Notification {
+public class ConfirmedDomainEvent<T> extends DomainEvent{
 
   public T DomainEvent;
 
   public ConfirmedDomainEvent(T domainEvent) {
-    this.DomainEvent = domainEvent;
+    this.DomainEvent = (T) domainEvent;
   }
 }
